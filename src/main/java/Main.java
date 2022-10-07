588c0699f344cd51a2f38c9cf2c5c58fbc29e47f
Neovodoo <75006634+Neovodoo@users.noreply.github.com>
2022-10-07 10:11:24 +0300

    Финальная версия (чистовой вариант)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import net.bytebuddy.asm.Advice;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
@@ -28,9 +27,14 @@ public class Main {
         */
 
         //Выполнил с прямой ссылкой на Яндекс маркет
-       try {
+
+        try {
             ChromeDriver driver = new ChromeDriver();
+
+            //задаю ожидание, чтобы страница успевала прогружаться
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+            //окрываю браузер на весь экран и перехожу по ссылке
             driver.get("https://market.yandex.ru/");
             driver.manage().window().maximize();
 
@@ -62,44 +66,58 @@ public class Main {
             WebElement diagonal = driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[3]/div[5]/div/div/div/div/aside/div/div[4]/div/div/div/div/div[13]/div/fieldset/div/div/div/span[1]/div/div/input"));
             diagonal.sendKeys("3");
 
+            //Ещё одна задержка (на всякий случай)
             Thread.sleep(5000);
 
 
-
+            //Создаю список из имеющихся элементов на странице
             List<WebElement> search_results = driver.findElements(By.xpath("//article[@class='_2vCnw cia-vs cia-cs']"));
 
+
+            //Условие для проверки того, что на странице более 10 элементов
             if (search_results.size()>=10) {
+
+                //Запоминаю первый элемент на странице
+                Thread.sleep(1000);
                 WebElement first_smartphone = driver.findElement(By.xpath("//span[@data-tid='2e5bde87']"));
-                Thread.sleep(5000);
+
+                //Преобразовываю первый элемент в текстовое значение
                 String  check = first_smartphone.getText();
-                System.out.println(check);
+                System.out.println("Выбранный смартфон: " + check);
+
+                //Переход к другой системе ранжирования (по рейтингу)
                WebElement sorting = driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[3]/div[5]/div/div/div/div/div/div/div[3]/div/div/div/div[1]/div/div/div/button[3]"));
                move.moveToElement(sorting).click().build().perform();
-                Thread.sleep(5000);
+               Thread.sleep(5000);
 
-
+                //Локатор на конец страницы (список страниц)
                 WebElement ending_of_page = driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[3]/div[5]/div/div/div/div/div/div/div[7]/div/div/div[2]"));
+
+                //Локатор на кнопку для переключения страниц ("Показать ещё")
                 WebElement new_page = driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[3]/div[5]/div/div/div/div/div/div/div[7]/div/div/div[1]/div/button/span"));
                 move.moveToElement(ending_of_page).build().perform();
-                int count = 1;
+
 
                 boolean present = false;
 
+                //Цикл для перехода на следующую страницу списка (в случае если искомого объекта нет на первой)
                 while (!present){
 
                     try {
+                        //Поиск нужного элемента на странице по названию и переход на него
                         WebElement smartphone_needed = driver.findElement(By.linkText(check));
                         move.moveToElement(smartphone_needed).click().build().perform();
 
                         present = true;
 
                     } catch (NoSuchElementException e) {
+                        //Переход на новую страницу (с прогрузкой всех элементов), если мы не находим нужный
                         Thread.sleep(1000);
                         move.moveToElement(new_page).click().build().perform();
                         Thread.sleep(1000);
                         move.moveToElement(ending_of_page).build().perform();
                         present = false;
-                        count++;
+
                     }
                     if (present) {
                         break;
@@ -107,11 +125,19 @@ public class Main {
                 }
             }
 
+            //Переключение на открытую вкладку смартфона
             List<String> change_tabs = new ArrayList<>(driver.getWindowHandles());
             driver.switchTo().window(change_tabs.get(1));
+
+            //Получение текстового значения рейтинга и его отображение
             WebElement raiting = driver.findElement(By.xpath("//span[@class='_2v4E8']"));
             String answer = raiting.getAttribute("innerText");
-            System.out.println(answer);
+
+            //Вывод ответа и доп информации
+            System.out.println("Значение рейтинга: " + answer);
+            driver.quit();
+
+
 
         }
         catch(InterruptedException e){
